03d3cea29b5ae6eff4d7b64ada00b8a8385926ac
Akhildeepreddy <akhildeepreddy@gmail.com>
2017-03-21 20:54:52 +0530

    Add files via upload

---
+++ S1_21_3/Sample.java
@@ -0,0 +1,14 @@
+package oops.S1_21_3;
+
+
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I work for mouri tech");
+
+	}
+
+}
